e7eb596d0b029b9f3338ada4c041d9ad047c732b
Andrew Moles <andrewmoles2@gmail.com>
2023-08-18 15:54:00 +0100

    add video solutions

---
+++ resources/solutions/exercises.R
@@ -0,0 +1,22 @@
+# Waist hip ratio calculation
+
+# assign variables of waist and hip
+waist <- 90
+hip <- 107
+
+# calculation of waist-to-hip ratio
+whr <- waist/hip
+
+# print outcome
+whr
+paste0("Waist to hip ratio of ", whr)
+
+# Exercise: BMI calculation
+# your code here ----
+# assign variables
+weight <- 94
+height <- 1.95
+# calculate bmi
+bmi <- weight/(height^2)
+# print result
+bmi
---
+++ resources/solutions/r-pre-sessional.R
@@ -0,0 +1,18 @@
+# running time in minutes
+run_minutes <- 26.34
+# running time in seconds
+run_seconds <- run_minutes*60
+# print result
+run_seconds
+
+# height conversion
+my_height <- 195
+
+my_height_ft <- my_height * 0.0328084
+my_height_ft
+
+my_height_m <- my_height / 100
+my_height_m
+
+my_height_ft_m <- my_height_ft * 0.3048
+my_height_ft_m
